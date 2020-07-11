3224475107ad8917cf9c4d48c1f94e2d08fc1cf1
coding-omelet <none>
2020-07-11 12:18:47 +0900

    本登録ページの作成、多重登録対策

---
+++ register_page_2.php
@@ -0,0 +1,83 @@
+<!-- メンバーテーブルの構成を変更する（ユーザー名） -->
+<?php
+    $errors = array();
+
+    // メールから遷移していない場合
+    if (!isset($_GET["urltoken"])) {
+        // ユーザー登録ページに飛ばす
+        header("Location: register_page.php");
+        exit;
+
+    // メールから遷移している場合
+    } else {
+        $urltoken = $_GET["urltoken"];
+
+        // データベースに接続
+        require_once 'db.php';
+
+        // urltokenに一致し、未登録かつ仮登録から24時間以内のレコードを取り出す
+        // 同一アドレスからは複数の登録はないとする
+        $sql = 'SELECT mail FROM pre_member WHERE urltoken=:urltoken AND flag=0 AND date > now() - interval 24 hour';
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(':urltoken', $urltoken, PDO::PARAM_STR);
+        $stmt->execute();
+
+        // 取り出したレコード数
+        $row_count = $stmt->rowCount();
+
+        // レコード数が0なら、期限切れまたは存在しないトークンまたは本登録済み
+        if ($row_count == 0) {
+            $errors['expire'] = 'エラーが発生しました。次のような原因が考えられます：<br>'
+            .'リンクの有効期限が切れている。<br>'
+            .'既にユーザー登録が完了している。<br>'
+            .'無効なリンクを使用している。';
+        
+        // レコード数が複数なら、全く同時に複数人に同じトークンが発行された
+        } elseif ($row_count > 1) {
+            $errors['double'] = 'エラーが発生したため、初めから登録をやり直してください。';
+
+        // レコード数が1なら、メールアドレスを変数に格納
+        } else {
+            $mail_array = $stmt->fecth();
+            $mail = $mail_array[mail];
+        }
+    }
+?>
+
+<!-- ここから表示部分 -->
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title><?php echo count($errors) ? "エラー" : "ユーザー登録"; ?></title>
+    </head>
+    <body>
+        <!-- エラーがある場合 -->
+        <?php if (count($errors)): ?>
+            <?php
+                foreach ($errors as $error) {
+                    echo $error."<br>";
+                }
+            ?>
+        <!-- エラーがない場合 -->
+        <?php else: ?>
+            以下の事項を記入してください。
+            <form action="register_check.php" method="post">
+                <table>
+                    <tr>
+                        <td>ユーザー名（表示名）：<td>
+                        <td><input type="text" name="name"></td>
+                    </tr>
+                    <tr>
+                        <td>パスワード（半角英数字）：<td>
+                        <td><input type="password" name="password"></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td><input type="submit" value="確認画面へ"></td>
+                    </tr>
+                </table>
+            </form>
+        <?php endif; ?>
+    </body>
+</html>
--- register_send.php
+++ register_send.php
@@ -1,3 +1,5 @@
+<!-- 本登録が済んでいないかチェック -->
+
 <?php
     $errors = array();
 
@@ -21,24 +23,72 @@
         }
     }
 
-    // エラーがなければ
+    // 正しい形式のメールアドレスが入力されていれば
     if (count($errors) == 0) {
-        // クリック用アドレスを生成
-        $urltoken = hash('sha256', uniqid(rand(), 1));
-        $url = 'https://tb-220042.tech-base.net/hoge.php'.'?urltoken='.$urltoken;
-
         // データベースに接続
         require_once 'db.php';
 
-        // 仮登録テーブルに登録
-        $sql = 'INSERT INTO pre_member (urltoken, mail, date) VALUES (:urltoken, :mail, now())';
-        $stmt = $pdo -> prepare($sql);
-        $stmt -> bindParam(':urltoken', $urltoken, PDO::PARAM_STR);
-        $stmt -> bindParam(':mail', $mail_address, PDO::PARAM_STR);
-        $stmt -> execute();
+        // 他と被らないようにトークンを生成
+        while (TRUE) {
+            $urltoken = hash('sha256', uniqid(rand(), 1));
 
-        // メール送信
-        require 'phpmailer/send_register_mail.php';
+            // urltokenに一致し、未登録かつ仮登録から24時間以内のレコードを取り出す
+            $sql = 'SELECT flag FROM pre_member WHERE urltoken=:urltoken AND flag=0 AND date > now() - interval 24 hour';
+            $stmt = $pdo->prepare($sql);
+            $stmt->bindParam(':urltoken', $urltoken, PDO::PARAM_STR);
+            $stmt->execute();
+
+            // 取り出したレコード数が0なら、トークンは被っていない
+            $row_count = $stmt->rowCount();
+            if ($row_count == 0) break;
+        }
+        // クリック用アドレス
+        $url = 'https://tb-220042.tech-base.net/hoge.php'.'?urltoken='.$urltoken;
+
+
+        // 既に同じメールアドレスで本登録されていないか？
+
+        $sql = 'SELECT flag FROM member WHERE mail=:mail AND flag=1';
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(':mail', $mail_address, PDO::PARAM_STR);
+        $stmt->execute();
+        $row_count = $stmt->rowCount();
+
+        // 既に本登録されている場合
+        if ($row_count) {
+            $errors["mail_used"] = "入力されたメールアドレスは既に使用されています。ログインしてください。";
+        
+        // 本登録されていない場合
+        } else {
+            // 過去に仮登録されていないか？
+            $sql = 'SELECT flag FROM pre_member WHERE mail=:mail';
+            $stmt = $pdo->prepare($sql);
+            $stmt->bindParam(':mail', $mail_address, PDO::PARAM_STR);
+            $stmt->execute();
+            $row_count = $stmt->rowCount();
+            // 過去に仮登録されている場合
+            if ($row_count) {
+                // 仮登録テーブルの内容を更新
+                $sql = 'UPDATE pre_member SET urltoken=:urltoken,date=now(),flag=0 WHERE mail=:mail';
+                $stmt = $pdo->prepare($sql);
+                $stmt->bindParam(':urltoken', $urlroken, PDO::PARAM_STR);
+                $stmt->bindParam(':mail', $mail_address, PDO::PARAM_STR);
+                $stmt->execute();
+    
+            // 過去に仮登録されていない場合
+            } else {
+                // 仮登録テーブルに登録
+                $sql = 'INSERT INTO pre_member (urltoken, mail, date) VALUES (:urltoken, :mail, now())';
+                $stmt = $pdo -> prepare($sql);
+                $stmt -> bindParam(':urltoken', $urltoken, PDO::PARAM_STR);
+                $stmt -> bindParam(':mail', $mail_address, PDO::PARAM_STR);
+                $stmt -> execute();
+            }
+
+            // メール送信
+            require 'phpmailer/send_register_mail.php';
+        }
+
     }
 ?>
 
@@ -63,7 +113,5 @@
         <!-- エラーがない場合 -->
         入力いただいたメールアドレス宛にユーザー登録用のメールを送信しました。<br>
         メールに記載のリンクからユーザー登録を完了させてください。<br>
-        <br>
-        メールが届かない場合は、お手数ですが初めから登録をやり直してください。
     </body>
 </html>
